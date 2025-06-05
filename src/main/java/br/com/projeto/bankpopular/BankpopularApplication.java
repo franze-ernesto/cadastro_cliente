53b081845f878ea8ef8f6240457ef86866b9f733
franze-ernesto <ernesto.franzee@gmail.com>
2025-06-05 16:22:38 -0300

    Primeiro commit

---
+++ src/main/java/br/com/projeto/bankpopular/BankpopularApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bankpopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankpopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankpopularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/bankpopular/BankpopularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bankpopular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankpopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
